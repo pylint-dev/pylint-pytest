8edad6065f348759d20583b570db9156dba2bee7
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2023-10-23 17:21:45 +0300

    Fix tests running via PyCharm
    Would've been better to get the path via `requests.config.rootdir`,
    but it looks like a pain to inject the `requests` fixture here.
    
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- tests/base_tester_test.py
+++ tests/base_tester_test.py
@@ -1,7 +1,12 @@
 import pytest
-from base_tester import BasePytestTester
+from base_tester import BasePytestTester, get_test_root_path
 
 # pylint: disable=unused-variable
+
+
+def test_get_test_root_path():
+    assert get_test_root_path().name == "tests"
+    assert (get_test_root_path() / "input").is_dir()
 
 
 def test_init_subclass_valid_msg_id():
