f8064a3eb01c5b1814e74698fb64e8cc20ceec86
Anis Da Silva Campos <anis@lumapps.com>
2023-12-01 10:30:58 +0100

    Support pylint v3
    in pylint v3.0.0, there is https://github.com/pylint-dev/pylint/pull/8404, that breaks the current implementation of:
    - ClassAttrLoader
    - FixtureChecker
    This fix is an attempt of supporting both pylint v2 and v3

--- pylint_pytest/checkers/__init__.py
+++ pylint_pytest/checkers/__init__.py
@@ -1,5 +1,13 @@
+import pylint
 from pylint.checkers import BaseChecker
 
 
 class BasePytestChecker(BaseChecker):
+    if int(pylint.version.split(".")[0]) < 3:
+        # Since https://github.com/pylint-dev/pylint/pull/8404, pylint does not need this
+        # __implements__ pattern. keeping it for retro compatibility with pylint==2.x
+        from pylint.interfaces import IAstroidChecker  # pylint: disable=import-outside-toplevel
+
+        __implements__ = IAstroidChecker
+
     name = "pylint-pytest"
