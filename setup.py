05e6359881fc1f849e3de46b38ff833731e3db2b
Stavros Ntentos <133706+stdedos@users.noreply.github.com>
2023-12-03 22:10:31 +0200

    Copy-paste those changes from a different branch
    Signed-off-by: Stavros Ntentos <133706+stdedos@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Only a compatibility placeholder
+https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+"""
+
 from setuptools import setup
 
 setup()
